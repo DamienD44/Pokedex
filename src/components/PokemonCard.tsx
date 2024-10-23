650df9b8e7b0c7fbbefd2a04a5dbd14b269c7036
DamienD44 <damas44@gmail.com>
2024-10-23 10:08:56 +0200

    quete 3 react

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,9 +1,35 @@
-function PokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokecard"></img>
 
-        <figcaption>Bulbasaur</figcaption>
-    </figure>;
+function PokemonCard() {
+    const pokemon = pokemonList[0];
+
+
+    return (
+        <figure>
+            {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc} alt={pokemon.name} />
+            ) : (
+                <p>???</p>
+            )}
+
+
+
+            <figcaption>pokemon.name</figcaption>
+        </figure>)
+
 }
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+
+
 
 export default PokemonCard;
