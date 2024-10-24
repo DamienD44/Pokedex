62db659d849dc226ee02558289c41065b9274af0
DamienD44 <damas44@gmail.com>
2024-10-24 12:20:06 +0200

    quete 4 react

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,17 +1,9 @@
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
 
-function PokemonCard() {
 
-    const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
+
+
+
 
     return (
 
